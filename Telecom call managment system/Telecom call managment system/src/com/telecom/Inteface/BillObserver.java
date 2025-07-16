9c547569895dab59bc28cb058c0f9f3309bea044
Kiruthika-Sekar123 <kiruthikasece2002@gmail.com>
2025-07-16 17:03:32 +0530

    Delete Telecom call managment system/Telecom call managment system directory

--- Telecom call managment system/Telecom call managment system/src/com/telecom/Inteface/BillObserver.java
+++
@@ -1,8 +0,0 @@
-package com.telecom.Inteface;
-
-import com.telecom.model.Customer;
-
-public interface BillObserver {
-    void update(Customer customer, double amount);
-}
-
--- Telecom call managment system/Telecom call managment system/src/com/telecom/Inteface/Plan.java
+++
@@ -1,8 +0,0 @@
-package com.telecom.Inteface;
-
-
-public interface Plan {
-    double calculateBill(long seconds);
-}
-
-
